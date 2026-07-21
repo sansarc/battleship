33c3dd292f962048982f0f9c3f52cbd28de9c321
sansarc <sansevieroarcangelo@gmail.com>
2026-07-21 11:20:46 +0200

    feat(server): define core structs, macros, and function prototypes

---
+++ server/include/game.h
@@ -0,0 +1,18 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "server.h"
+
+void handle_client_message(Client *client, char *message);
+void create_match(Client* client);
+void list_matches(const Client* client);
+void request_join_match(Client *client, int match_id);
+void handle_join_response(const Client *client, int match_id, int accepted);
+void place_ship(const Client *client, int match_id, int x, int y, int length, char orientation);
+void handle_shoot(const Client *client, int match_id, int x, int  y);
+
+void handle_rematch(const Client *client, int match_id);
+void handle_leave(const Client *client, int match_id);
+void cleanup_client_matches(const Client *client);
+
+#endif
---
+++ server/include/server.h
@@ -0,0 +1,46 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <pthread.h>
+#include <netinet/in.h>
+
+#define MAX_CLIENTS 100
+#define MAX_GAMES 50
+#define GRID_SIZE 10
+#define SHIPS_PER_PLAYER 3
+
+typedef enum {
+    GAME_WAITING,
+    GAME_PLACEMENT,
+    GAME_COMBAT,
+    GAME_FINISHED
+} GameState;
+
+typedef struct {
+    int socket_fd;
+    int is_connected;
+    int user_id;
+} Client;
+
+typedef struct {
+    int id;
+    Client* creator;
+    Client* opponent;
+    GameState state;
+    int creator_grid[GRID_SIZE][GRID_SIZE];
+    int opponent_grid[GRID_SIZE][GRID_SIZE];
+    int creator_ships_placed;
+    int opponent_ships_placed;
+    int creator_ready;
+    int opponent_ready;
+    int turn;
+    pthread_mutex_t lock;
+} Match;
+
+typedef struct {
+    Match games[MAX_GAMES];
+    int active_games_count;
+    pthread_mutex_t global_lock;
+} GameManager;
+
+#endif
